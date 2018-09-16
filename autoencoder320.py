a0bb3b8d81b45e2749bcd008d871a2ecbccc0cca
Hedi <hedixia@umail.ucsb.edu>
2018-09-16 11:52:20 +0800

    x

--- autoencoder320.py
+++ autoencoder320.py
@@ -23,13 +23,12 @@ M = "maxpool"
 U = "upsample"
 F = "final"
 
-aenn = ModelGenerator(Param.__dict__)
-aenn.add_layer(C, M, C, M, C, M, syntax="E")  # Encoder
-aenn.add_layer(C, U, U, U, F, syntax="D")  # Decoder
-open("model_summary.txt", "w").write(aenn.summary)
 
-class ae320 (ModelGenerator):
-	def __init__(self):
+class ae320(ModelGenerator):
+	def __init__ (self):
 		super().__init__(Param.__dict__)
 		self.add_layer(C, M, C, M, C, M, syntax="E")  # Encoder
 		self.add_layer(C, U, U, U, F, syntax="D")  # Decoder
+
+	def write_summary (self):
+		open("model_summary.txt", "w").write(self.summary)
